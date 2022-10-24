68e862851ff43193e4a1451ddb06847432f6be81
pyuan-21 <yuanputianatin@outlook.com>
2022-10-24 12:29:53 +0200

    update

--- Assets/Scripts/GameLogic.cs
+++ Assets/Scripts/GameLogic.cs
@@ -4,15 +4,50 @@ using UnityEngine;
 
 public class GameLogic : MonoBehaviour
 {
+    private GameObject buildingRoot;
+    private List<string> buildingNames;
+
+    public float environmentValue;
+    public float economyValue;
+
+    // TODO: change these two progressbar
+    //public ProgressB
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        buildingNames = new List<string>();
+        buildingNames.Add("food_factory");
+        buildingNames.Add("industrial_factory");
+        buildingRoot = new GameObject("Buildings");
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
+    }
+
+    public void OnAddBuilding(Vector3 pos)
+    {
+        int randomIndex = UnityEngine.Random.Range(0, buildingNames.Count - 1);
+        string buildingName = buildingNames[randomIndex];
+        GameObject prefab = Resources.Load<GameObject>("BuildingPrefabs/" + buildingName);
+        GameObject prefabInstance = Instantiate<GameObject>(prefab);
+        prefabInstance.transform.SetParent(buildingRoot.transform);
+        prefabInstance.transform.position = pos;
+        prefabInstance.transform.rotation = Quaternion.identity;
+        prefabInstance.transform.localScale = Vector3.one;
+
+        economyValue += 5;
+        economyValue = Mathf.Clamp(economyValue, 0, 100);
+
+        environmentValue -= 5;
+        environmentValue = Mathf.Clamp(environmentValue, 0, 100);
+    }
+
+    public void OnAddEnvironment(Vector3 pos)
+    {
+
     }
 }
---
+++ Assets/Scripts/InteractHandler.cs
@@ -0,0 +1,91 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR;
+using UnityEngine.XR.Interaction.Toolkit;
+
+public class InteractHandler : MonoBehaviour
+{
+    private XRGrabInteractable grable;
+    private GameLogic game;
+    private bool removeItself;
+    public bool test = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if(gameObject.TryGetComponent<XRGrabInteractable>(out grable))
+        {
+            grable.onSelectEnter.AddListener(this.EnterSelect);
+            grable.onSelectExit.AddListener(this.ExitSelect);
+        }
+        var temp = GameObject.Find("XR Rig");
+        if (temp != null)
+        {
+            temp.TryGetComponent<GameLogic>(out game);
+        }
+        removeItself = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(removeItself)
+        {
+            removeItself = false;
+            Destroy(gameObject);
+        }
+
+        // just test
+        if (test)
+        {
+            test = false;
+            EnterSelect(null);
+        }
+    }
+
+    public void EnterSelect(XRBaseInteractor iter)
+    {
+        if (gameObject.CompareTag("Environment"))
+        {
+            Vector3 rayOrigin = transform.position + Vector3.up * 1000;
+            RaycastHit[] hits = Physics.RaycastAll(rayOrigin, Vector3.down, Mathf.Infinity);
+            if (hits != null && hits.Length > 0)
+            {
+                for (int i = 0; i < hits.Length; i++)
+                {
+                    if (hits[i].transform.CompareTag("Ground"))
+                    {
+                        Vector3 bornPos = new Vector3(transform.position.x, hits[i].point.y, transform.position.z);
+                        game.OnAddBuilding(bornPos);
+                        removeItself = true;
+                        break;
+                    }
+                }
+            }
+        }
+        else if (gameObject.CompareTag("Buildings"))
+        {
+            Vector3 rayOrigin = transform.position + Vector3.up * 1000;
+            RaycastHit[] hits = Physics.RaycastAll(rayOrigin, Vector3.down, Mathf.Infinity);
+            if (hits != null && hits.Length > 0)
+            {
+                for (int i = 0; i < hits.Length; i++)
+                {
+                    if (hits[i].transform.CompareTag("Ground"))
+                    {
+                        Vector3 bornPos = new Vector3(transform.position.x, hits[i].point.y, transform.position.z);
+                        game.OnAddEnvironment(bornPos);
+                        removeItself = true;
+                        break;
+                    }
+                }
+            }
+        }
+    }
+
+    public void ExitSelect(XRBaseInteractor iter)
+    {
+
+    }
+}
